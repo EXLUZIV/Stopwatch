eb5bd682adaab413cd0987b49ea94b7811b98a94
EXLUZIV <pegasfasmain.gmail.com>
2022-03-19 23:16:54 +0200

    code optimization, readability improvements

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,28 +13,19 @@ let status = 'stop';
 let i = 1;
 
 window.onload = function () {
+
 	const startElement = document.querySelector('#start')
 	startElement.addEventListener('click', start)
+	
 	const resetElement = document.querySelector('#reset')
 	resetElement.addEventListener('click', reset)
+	
 	const lapElement = document.querySelector('#lap')
 	lapElement.addEventListener('click', lap)
+
 }
 
-const stopWatch = () => {
-	milisecond++;
-	if (milisecond / 60 === 1) {
-		milisecond = 0;
-		second++;
-		if (second / 60 === 1) {
-			second = 0;
-			minut++
-			if (minut / 60 === 1) {
-				minut = 0;
-				hour++;
-			};
-		};
-	};
+let timeLessThan0 = () => {
 
 	if (milisecond <= 9) {
 		displayMilisecond = "0" + milisecond.toString();
@@ -59,20 +50,54 @@ const stopWatch = () => {
 	} else {
 		displayHour = hour;
 	};
+}
+
+const stopWatch = () => {
+
+	milisecond++;
+	
+	if (milisecond / 60 === 1) {
+
+		milisecond = 0;
+		second++;
+		
+		if (second / 60 === 1) {
+
+			second = 0;
+			minut++
+		
+			if (minut / 60 === 1) {
+
+				minut = 0;
+				hour++;
+			
+			};
+		};
+	};
+
+	timeLessThan0();
+
 	document.getElementById('display').innerHTML = displayHour + ':' + displayMinut + ':' + displaySecond + ':' + displayMilisecond;
 };
-const start = () =>{
+
+const start = () => {
+	
 	if (status === "stop"){
+
 		interval = window.setInterval(stopWatch, 10);
 		document.getElementById('start').innerHTML = "Stop";
 		status = 'start';
 	} else{
+
 		window.clearInterval(interval)
 		document.getElementById('start').innerHTML = "Start";
 		status = 'stop';
+	
 	};
 };
+
 const reset = () => {
+
 	window.clearInterval(interval);
 	milisecond = 0;
 	second = 0;
@@ -83,43 +108,30 @@ const reset = () => {
 	status = 'stop';
 	i = 1;
 	document.querySelectorAll('.newLap').forEach(e => e.remove());
+
 };
+
 let newLap = startLap = null;
+
 const lap = () => {
-	if (milisecond <= 9) {
-		displayMilisecond = "0" + milisecond.toString();
-	} else {
-		displayMilisecond = milisecond;
-	};
-
-	if (second <= 9) {
-		displaySecond = "0" + second.toString();
-	} else {
-		displaySecond = second;
-	};
-
-	if (minut <= 9 ){
-		displayMinut = "0" + minut.toString();
-	} else {
-		displayMinut = minut;
-	};
 	
-	if (hour <= 9 ){
-		displayHour = "0" + hour.toString();
-	} else {
-		displayHour = hour;
-	};
+	timeLessThan0();
+
 	let newLap = document.createElement('p');
-	if (i === 1)
-	{
+	
+	if (i === 1) {
+		
 		newLap.innerHTML = "<p class = 'newLap first'>" + i + ' lap ' + displayHour + ':' + displayMinut + ':' + displaySecond + ':' + displayMilisecond + "</p>";
 		startLap = document.getElementById('forLap');
 		document.body.insertBefore(newLap, startLap);
 	} else {
+		
 		newLap.innerHTML = "<p class = 'newLap'>" + i + ' lap ' + displayHour + ':' + displayMinut + ':' + displaySecond + ':' + displayMilisecond + "</p>";
 		startLap = document.getElementById('forLap');
 		document.body.insertBefore(newLap, startLap);
+	
 	}
 	
 	i++;
+
 };
